95c0228aa0daf52863b95d2ce4a29949128e51ee
Jacob Curry <jacob.a.curry@gmail.com>
2022-11-17 20:41:59 -0500

    movie search api working

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+let newMovie = "";
+
+const axiosInstance = axios.create({
+  baseURL: "https://www.omdbapi.com/?apikey=785aa019&t=",
+  header: { "Access-Control-Allow_Origin": "*" },
+});
 
 router.get("/", (req, res) => {
-  res.render("moviehome.ejs");
+  res.render("moviehome.ejs", {
+    currentUser: req.session.currentUser,
+    data: "",
+  });
+});
+router.get("/movie", async (req, res, next) => {
+  const movie = req.query.movie;
+  const response = await axiosInstance.get(movie);
+  newMovie = response.data;
+  res.render("moviehome.ejs", {
+    currentUser: req.session.currentUser,
+    data: newMovie,
+  });
+});
+
+router.get("/movie/show", (req, res) => {
+  res.render("movieshow.ejs", {
+    currentUser: req.session.currentUser,
+    data: newMovie,
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const session = require("express-session");
+
 const app = express();
 
-//movie router
-const movieRouter = require("./routes/movieRoute.js");
-app.use("/movie", movieRouter);
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//checking to see if user is logged in
+const isAuthenticated = (req, res, next) => {
+  if (req.session.currentUser) {
+    return next();
+  } else {
+  }
+};
+
+//user controller
+const userController = require("./controllers/users_controller.js");
+app.use("/users", userController);
+
+//session controller
+const sessionsConstoller = require("./controllers/sessions_controller.js");
+app.use("/sessions", sessionsConstoller);
 
 //staic folders
 app.use(express.static("public"));
+
+//movie router
+const movieRouter = require("./routes/movieRoute.js");
+app.use("/", movieRouter);
 
 let PORT = 3000;
 
